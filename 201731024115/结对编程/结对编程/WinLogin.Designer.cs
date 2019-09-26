67a262d9e08723461b3d04b60eeddd9d934ffad6
zenglang1 <905144053@qq.com>
2019-09-26 22:46:46 +0800

    第一次合并

---
+++ 201731024115/结对编程/结对编程/WinLogin.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace 结对编程
+{
+    partial class WinLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btmCancel = new System.Windows.Forms.Button();
+            this.btmLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btmCancel
+            // 
+            this.btmCancel.Location = new System.Drawing.Point(532, 282);
+            this.btmCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btmCancel.Name = "btmCancel";
+            this.btmCancel.Size = new System.Drawing.Size(74, 38);
+            this.btmCancel.TabIndex = 23;
+            this.btmCancel.Text = "取消";
+            this.btmCancel.UseVisualStyleBackColor = true;
+            // 
+            // btmLogin
+            // 
+            this.btmLogin.Location = new System.Drawing.Point(389, 282);
+            this.btmLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btmLogin.Name = "btmLogin";
+            this.btmLogin.Size = new System.Drawing.Size(74, 38);
+            this.btmLogin.TabIndex = 22;
+            this.btmLogin.Text = "登陆";
+            this.btmLogin.UseVisualStyleBackColor = true;
+            this.btmLogin.Click += new System.EventHandler(this.btmLogin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 197);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "密码";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(414, 187);
+            this.txtPwd.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(192, 28);
+            this.txtPwd.TabIndex = 20;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(414, 121);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(192, 28);
+            this.txtName.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 121);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "账号";
+            // 
+            // WinLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 440);
+            this.Controls.Add(this.btmCancel);
+            this.Controls.Add(this.btmLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "WinLogin";
+            this.Text = "WinLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected internal System.Windows.Forms.Button btmCancel;
+        protected internal System.Windows.Forms.Button btmLogin;
+        protected internal System.Windows.Forms.Label label2;
+        protected internal System.Windows.Forms.TextBox txtPwd;
+        protected internal System.Windows.Forms.TextBox txtName;
+        protected internal System.Windows.Forms.Label label1;
+    }
+}
