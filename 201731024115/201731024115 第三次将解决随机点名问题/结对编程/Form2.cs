b8b12311b5c6c79285b44e37268bd0cc75d7a098
zenglang1 <905144053@qq.com>
2019-09-27 17:29:54 +0800

    zenglang1

---
+++ 201731024115/201731024115 第三次将解决随机点名问题/结对编程/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 结对编程
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timerCallName.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timerCallName.Stop();
+        }
+
+        private void timerCallName_Tick(object sender, EventArgs e)
+        {
+            Random ra = new Random();
+            int i = ra.Next(30);
+            string sql = string.Format("select * from tblStu");
+            DataSet ds = connect.querysql(sql);
+            Label1.Text = Convert.ToString(ds.Tables[0].Rows[i]["stuName"]);
+            
+        }
+    }
+}
---
+++ 201731024115/201731024115 第三次将解决随机点名问题/结对编程/connect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace 结对编程
+{
+    class connect
+    {
+        public static int ExecuteSQL(string sql)
+        {
+            string str = "server=LAPTOP-K6RLAPNC;database=Demo;Trusted_Connection=true";
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+
+            SqlCommand comm = new SqlCommand(sql, conn);
+            int iRet = comm.ExecuteNonQuery();
+            conn.Close();
+            return iRet;
+        }
+        public static DataSet querysql(string sql)
+        {
+
+            SqlConnection conn = new SqlConnection("server=LAPTOP-K6RLAPNC;database=Demo;Trusted_Connection=true");
+            conn.Open();
+            SqlDataAdapter Adapter = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            Adapter.Fill(ds);
+            conn.Close();
+
+            return ds;
+        }
+    }
+}
