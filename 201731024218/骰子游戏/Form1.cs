0467fa4cd1337b40cd8d91f7cefa9750b3b5f533
shushanyalida5124 <785065948@qq.com>
2019-09-27 22:21:22 +0800

    删除了一些不必要的代码

--- 201731024218/骰子游戏/Form1.cs
+++ 201731024218/骰子游戏/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace 骰子游戏
@@ -14,8 +8,7 @@ namespace 骰子游戏
     {
         Touzi touzi = new Touzi();
         int DaOrXiao=1;          //表示猜测是大还是小，1为大，2为小
-        int result;             //表示结果是大还是小
-        string basePath;        //根目录路径
+        int result;             //表示结果是大还是小 
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +16,7 @@ namespace 骰子游戏
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            axWindowsMediaPlayer1.URL = basePath + @"音乐\卢冠廷 - 賭神.mp3";
+            axWindowsMediaPlayer1.URL =  @"音乐\卢冠廷 - 賭神.mp3";
         }
 
 
@@ -45,7 +38,7 @@ namespace 骰子游戏
         //点击开始是将BGM变为赌神，启用时钟
         private void BtnStart_Click(object sender, EventArgs e)
         {
-            axWindowsMediaPlayer1.URL = basePath+@"音乐\卢冠廷 - 賭神.mp3";
+            axWindowsMediaPlayer1.URL =@"音乐\卢冠廷 - 賭神.mp3";
             timer1.Enabled = true;
         }
 
@@ -54,31 +47,31 @@ namespace 骰子游戏
         private void Timer1_Tick(object sender, EventArgs e)
         {
             touzi.Dice();
-            string picPAth=basePath;
+            string picPAth;
             switch (touzi.Num)
             {
                 case 1:
-                    picPAth += @"图片\1.jpg";
+                    picPAth = @"图片\1.jpg";
                     result = 0;
                     break;
                 case 2:
-                    picPAth += @"图片\2.jpg";
+                    picPAth = @"图片\2.jpg";
                     result = 0;
                     break;
                 case 3:
-                    picPAth += @"图片\3.jpg";
+                    picPAth = @"图片\3.jpg";
                     result = 0;
                     break;
                 case 4:
-                    picPAth += @"图片\4.jpg";
+                    picPAth = @"图片\4.jpg";
                     result = 1;
                     break;
                 case 5:
-                    picPAth += @"图片\5.jpg";
+                    picPAth = @"图片\5.jpg";
                     result = 1;
                     break;
                 case 6:
-                    picPAth += @"图片\6.jpg";
+                    picPAth= @"图片\6.jpg";
                     result = 1;
                     break;
                 default:picPAth = null;
@@ -95,14 +88,14 @@ namespace 骰子游戏
             timer1.Enabled = false;
             if (result==DaOrXiao)
             {
-                axWindowsMediaPlayer1.URL =basePath+ @"音乐\Ozo - Epic Sax Guy.mp3";
-                Bitmap bm = new Bitmap(basePath+@"图片\win.gif");
+                axWindowsMediaPlayer1.URL = @"音乐\Ozo - Epic Sax Guy.mp3";
+                Bitmap bm = new Bitmap(@"图片\win.gif");
                 pictureBox2.Image = bm;
             }
             else
             {
-                axWindowsMediaPlayer1.URL = basePath+@"音乐\阿炳 - 二泉映月.mp3";
-                Bitmap bm = new Bitmap(basePath+@"图片\lose.gif");
+                axWindowsMediaPlayer1.URL = @"音乐\阿炳 - 二泉映月.mp3";
+                Bitmap bm = new Bitmap(@"图片\lose.gif");
                 pictureBox2.Image = bm;
             }
         }
