ff17e1573bd12eac4d83c6ea5e1089150cf32cd3
Yanyixiao <xiaoxiaotropic@qq.com>
2019-09-28 13:13:05 +0800

    ver0.1 写了一点点

---
+++ 201731023225/SelectStudent/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SelectStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Read = new System.Windows.Forms.Button();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.Select = new System.Windows.Forms.Button();
+            this.Name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Read
+            // 
+            this.Read.Location = new System.Drawing.Point(289, 167);
+            this.Read.Name = "Read";
+            this.Read.Size = new System.Drawing.Size(92, 23);
+            this.Read.TabIndex = 0;
+            this.Read.Text = "读取学生文件";
+            this.Read.UseVisualStyleBackColor = true;
+            this.Read.Click += new System.EventHandler(this.Read_Click);
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(12, 169);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(271, 21);
+            this.TxtName.TabIndex = 1;
+            // 
+            // Select
+            // 
+            this.Select.Location = new System.Drawing.Point(289, 140);
+            this.Select.Name = "Select";
+            this.Select.Size = new System.Drawing.Size(91, 23);
+            this.Select.TabIndex = 0;
+            this.Select.Text = "随机点名";
+            this.Select.UseVisualStyleBackColor = true;
+            this.Select.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Font = new System.Drawing.Font("宋体", 80F);
+            this.Name.Location = new System.Drawing.Point(12, 9);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(369, 107);
+            this.Name.TabIndex = 2;
+            this.Name.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 208);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.Select);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.Read);
+            this.Name = "Form1";
+            this.Text = "随机点名软件";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Read;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.Button Select;
+        private System.Windows.Forms.Label Name;
+    }
+}
+
---
+++ 201731023225/SelectStudent/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+
+namespace SelectStudent
+{
+    public partial class Form1 : Form
+    {
+
+        string[] StudentName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Read_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            OFD.Filter = "(*.txt)|*.txt";
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                string strNames = OFD.FileName;
+                //将文件名添加到 textbox中
+                TxtName.Text = strNames;
+            }
+            StudentName = File.ReadAllLines(TxtName.Text);
+            //读取完成txt中的姓名，txt中姓名由换行隔开
+        }
+
+        private void Select_Click(object sender, EventArgs e)
+        {
+            Random RD= new Random();
+
+            Name.Text=
+        }
+    }
+}
