648fd27b603349759922b1d0f06c386051c8c569
Ht1567 <206114364@qq.com>
2019-09-29 12:53:11 +0800

    First Commit

---
+++ 201731024119/GisWinformPractice/WinformControlUse/WinformControlUse/登录.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinformControlUse
+{
+    public partial class 登录 : Form
+    {
+        public 登录()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = this.textBox1.Text;
+            string password = this.textBox2.Text;
+            if (name.Equals("201731024119") && password.Equals("123456"))
+            {
+                MessageBox.Show("登录成功");
+                FrmStudent frm = new FrmStudent();
+                frm.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("登录失败");
+            }
+        }
+    }
+}
