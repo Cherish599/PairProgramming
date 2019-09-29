24c36996fef88b0d7cbb9efb249ac8e7947ae77d
zenglang1 <905144053@qq.com>
2019-09-29 20:48:45 +0800

    最终版

---
+++ 201731024115/最终版/结对编程/connect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace 结对编程
+{
+  public  class connect
+    { 
+        public static DataSet querysql(string sql)
+        {
+
+            SqlConnection conn = new SqlConnection("server=LAPTOP-K6RLAPNC;database=Demo;Trusted_Connection=true");
+            conn.Open();
+            SqlDataAdapter Adapter = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            Adapter.Fill(ds);
+            conn.Close();
+
+            return ds;
+        }
+    }
+}
---
+++ 201731024115/最终版/结对编程Tests/Form2Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using 结对编程;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace 结对编程.Tests
+{
+    [TestClass()]
+    public class Form2Tests
+    {
+        [TestMethod()]
+        public void timerCallName_TickTest()
+        {
+            string sql = string.Format("select * from tblStu");
+            DataSet ds = connect.querysql("select * from tblStu");
+            string A = Convert.ToString(ds.Tables[0].Rows[11]["stuName"]);
+            StringAssert.Contains("张焱菁", A);
+            //Assert.Fail();     
+        }
+    }
+}
