b5251785060371ea1352e3305bf8bdef212a578f
John Doe <152056386@qq.com>
2019-09-29 22:42:59 +0800

    DataBase and Entity实现提交

---
+++ fantasysongs/第三次作业/WinformControlUse201731024138/Entity/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public class Student
+    {
+        private String num;
+
+        public String Num
+        {
+            get { return num; }
+            set { num = value; }
+        }
+        
+        private String id;
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+    
+
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Student()
+        { 
+        
+        }
+        public Student(String num,String id, String name)
+        {
+            this.num = num;
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
