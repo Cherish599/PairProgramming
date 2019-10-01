57647735a45b5267b215541096f4a2a2f705e615
Rabbit_Tu <794401381@qq.com>
2019-10-01 12:46:38 +0800

    Rabbit_Tu

---
+++ Rabbit-Tu/WinformControlUse/WinformControlUse/interface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinformControlUse
+{
+    public partial class @interface : Form
+    {
+        public @interface()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmStudent  frm = new FrmStudent ();
+            frm.Owner = this;
+            this.Hide();
+            frm.ShowDialog();
+
+        }
+    }
+}
---
+++ Rabbit-Tu/WinformControlUse/interface/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinformControlUse;
+
+namespace @interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TransparencyKey = Color.Red;
+            this.BackColor = Color.Red;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmStudent frm = new FrmStudent();
+            frm.Show();
+        }
+    }
+}
