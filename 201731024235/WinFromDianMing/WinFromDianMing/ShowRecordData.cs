1cb2dad7958c12932ebb6fb6a07eb4216dbd3115
Lobooi <54851944+Lobooi@users.noreply.github.com>
2019-10-01 11:30:20 +0800

    24235

---
+++ 201731024235/WinFromDianMing/WinFromDianMing/ShowRecordData.cs
@@ -0,0 +1,37 @@
+﻿using RecordData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFromDianMing
+{
+    public partial class ShowRecordData : Form
+    {
+        string data = null;
+        public ShowRecordData(string data)
+        {
+            InitializeComponent();
+            this.data = data;
+        }
+
+        private void ShowRecordData_Load(object sender, EventArgs e)
+        {
+           lblRCdata.Text = data;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            data = null;
+            lblRCdata.Text = data;
+            DialogResult = DialogResult.OK;
+            btnClear.Enabled = false;
+        }
+
+    }
+}
