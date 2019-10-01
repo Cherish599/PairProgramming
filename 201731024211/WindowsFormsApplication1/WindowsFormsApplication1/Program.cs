82e14b78d2af42747168bc56d7d5e0799af5cf3c
zerowcl <2375883623@qq.com>
2019-10-01 11:39:45 +0800

    201731024211

---
+++ 201731024211/WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            FrmLogin  f1 = new FrmLogin ();
+            Application.Run(f1);
+            if (f1.Tag.ToString() == "验证成功")
+            {
+                FrmStudent  f2 = new FrmStudent ();
+                Application.Run(f2);
+            }
+            else
+            {
+                Application.Exit();
+            }
+
+        }
+    }
+}
