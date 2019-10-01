454df0cd225a97cc24296654263a5228c09ff807
Your Name <you@example.com>
2019-10-01 14:59:00 +0800

    结队24229

---
+++ 201731024235/WinFromDianMing/EntityTests/StudentTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Tests
+{
+    [TestClass()]
+    public class StudentTests
+    {
+        [TestMethod()]
+        public void StudentTest()
+        {
+
+        }
+    }
+}
