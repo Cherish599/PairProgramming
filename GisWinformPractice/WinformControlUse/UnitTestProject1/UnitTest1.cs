07ae0c61f2afa8998108c04cc550a0363532e295
bokdw <43521974@qq.com>
2019-10-01 20:14:04 +0800

    姜仪201731024206

---
+++ GisWinformPractice/WinformControlUse/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entity;
+using System.Collections.Generic;
+using DaoLayer;
+using DataBase;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+          public static Student studengname;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StudentDAO jy = new StudentDAO();
+            StudentDataset wmy = new StudentDataset();
+            Student ct = new Student();
+        }
+    }
+}
