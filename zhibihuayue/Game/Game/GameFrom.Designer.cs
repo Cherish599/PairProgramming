c75f72a8aabc504e844ba4efa8c6b5b9365dc37e
zhibihuayue <wuxiandaizhi@163.com>
2019-10-01 21:29:09 +0800

    于丁201731021227

---
+++ zhibihuayue/Game/Game/GameFrom.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace Game
+{
+    partial class GameFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Dice6 = new System.Windows.Forms.Button();
+            this.Dice3 = new System.Windows.Forms.Button();
+            this.Dice4 = new System.Windows.Forms.Button();
+            this.Dice5 = new System.Windows.Forms.Button();
+            this.Dice2 = new System.Windows.Forms.Button();
+            this.Dice1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.gues = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.Num = new System.Windows.Forms.Label();
+            this.Rollpic = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Res = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Rollpic)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 42);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("楷体", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 45);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "欢迎来到投骰子游戏";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Dice6);
+            this.panel2.Controls.Add(this.Dice3);
+            this.panel2.Controls.Add(this.Dice4);
+            this.panel2.Controls.Add(this.Dice5);
+            this.panel2.Controls.Add(this.Dice2);
+            this.panel2.Controls.Add(this.Dice1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 42);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(214, 408);
+            this.panel2.TabIndex = 1;
+            // 
+            // Dice6
+            // 
+            this.Dice6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Dice6.Font = new System.Drawing.Font("微软雅黑", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Dice6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Dice6.Location = new System.Drawing.Point(105, 231);
+            this.Dice6.Name = "Dice6";
+            this.Dice6.Size = new System.Drawing.Size(53, 64);
+            this.Dice6.TabIndex = 13;
+            this.Dice6.Text = "6";
+            this.Dice6.UseVisualStyleBackColor = false;
+            this.Dice6.Click += new System.EventHandler(this.Dice6_Click);
+            // 
+            // Dice3
+            // 
+            this.Dice3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Dice3.Font = new System.Drawing.Font("微软雅黑", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Dice3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Dice3.Location = new System.Drawing.Point(24, 145);
+            this.Dice3.Name = "Dice3";
+            this.Dice3.Size = new System.Drawing.Size(53, 64);
+            this.Dice3.TabIndex = 12;
+            this.Dice3.Text = "3";
+            this.Dice3.UseVisualStyleBackColor = false;
+            this.Dice3.Click += new System.EventHandler(this.Dice3_Click);
+            // 
+            // Dice4
+            // 
+            this.Dice4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Dice4.Font = new System.Drawing.Font("微软雅黑", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Dice4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Dice4.Location = new System.Drawing.Point(105, 145);
+            this.Dice4.Name = "Dice4";
+            this.Dice4.Size = new System.Drawing.Size(53, 64);
+            this.Dice4.TabIndex = 11;
+            this.Dice4.Text = "4";
+            this.Dice4.UseVisualStyleBackColor = false;
+            this.Dice4.Click += new System.EventHandler(this.Dice4_Click);
+            // 
+            // Dice5
+            // 
+            this.Dice5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Dice5.Font = new System.Drawing.Font("微软雅黑", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Dice5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Dice5.Location = new System.Drawing.Point(24, 231);
+            this.Dice5.Name = "Dice5";
+            this.Dice5.Size = new System.Drawing.Size(53, 64);
+            this.Dice5.TabIndex = 10;
+            this.Dice5.Text = "5";
+            this.Dice5.UseVisualStyleBackColor = false;
+            this.Dice5.Click += new System.EventHandler(this.Dice5_Click);
+            // 
+            // Dice2
+            // 
+            this.Dice2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Dice2.Font = new System.Drawing.Font("微软雅黑", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Dice2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Dice2.Location = new System.Drawing.Point(105, 57);
+            this.Dice2.Name = "Dice2";
+            this.Dice2.Size = new System.Drawing.Size(53, 64);
+            this.Dice2.TabIndex = 9;
+            this.Dice2.Text = "2";
+            this.Dice2.UseVisualStyleBackColor = false;
+            this.Dice2.Click += new System.EventHandler(this.Dice2_Click);
+            // 
+            // Dice1
+            // 
+            this.Dice1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Dice1.Font = new System.Drawing.Font("微软雅黑", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Dice1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Dice1.Location = new System.Drawing.Point(24, 57);
+            this.Dice1.Name = "Dice1";
+            this.Dice1.Size = new System.Drawing.Size(53, 64);
+            this.Dice1.TabIndex = 8;
+            this.Dice1.Text = "1";
+            this.Dice1.UseVisualStyleBackColor = false;
+            this.Dice1.Click += new System.EventHandler(this.Dice1_Click_1);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.gues);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.gus);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 320);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(214, 88);
+            this.panel3.TabIndex = 7;
+            // 
+            // gues
+            // 
+            this.gues.AutoSize = true;
+            this.gues.Font = new System.Drawing.Font("宋体", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.gues.Location = new System.Drawing.Point(38, 33);
+            this.gues.Name = "gues";
+            this.gues.Size = new System.Drawing.Size(99, 40);
+            this.gues.TabIndex = 3;
+            this.gues.Text = "预测";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Click += new System.EventHandler(this.Label3_Click_1);
+            // 
+            // gus
+            // 
+            this.gus.AutoSize = true;
+            this.gus.Location = new System.Drawing.Point(77, 45);
+            this.gus.Name = "gus";
+            this.gus.Size = new System.Drawing.Size(0, 15);
+            this.gus.TabIndex = 1;
+            this.gus.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "你的预测";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选择你猜测的点数";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.Num);
+            this.panel4.Controls.Add(this.Rollpic);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(214, 42);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(276, 408);
+            this.panel4.TabIndex = 2;
+            // 
+            // Num
+            // 
+            this.Num.AutoSize = true;
+            this.Num.Font = new System.Drawing.Font("宋体", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Num.Location = new System.Drawing.Point(50, 307);
+            this.Num.Name = "Num";
+            this.Num.Size = new System.Drawing.Size(181, 40);
+            this.Num.TabIndex = 4;
+            this.Num.Text = "投掷点数";
+            // 
+            // Rollpic
+            // 
+            this.Rollpic.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Rollpic.Image = global::Game.Properties.Resources.timg;
+            this.Rollpic.Location = new System.Drawing.Point(0, 62);
+            this.Rollpic.Name = "Rollpic";
+            this.Rollpic.Size = new System.Drawing.Size(276, 291);
+            this.Rollpic.TabIndex = 3;
+            this.Rollpic.TabStop = false;
+            this.Rollpic.Visible = false;
+            this.Rollpic.Click += new System.EventHandler(this.Rollpic_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "丢骰子";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button2);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(0, 353);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(276, 55);
+            this.panel5.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(78, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 28);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "开始新的游戏";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.label4);
+            this.panel6.Controls.Add(this.Res);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(490, 42);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(310, 408);
+            this.panel6.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(255, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "游戏流程:预测点数→丢骰子→下一局";
+            this.label4.Click += new System.EventHandler(this.Label4_Click_1);
+            // 
+            // Res
+            // 
+            this.Res.AutoSize = true;
+            this.Res.BackColor = System.Drawing.Color.Red;
+            this.Res.Font = new System.Drawing.Font("微软雅黑", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Res.Location = new System.Drawing.Point(27, 326);
+            this.Res.Name = "Res";
+            this.Res.Size = new System.Drawing.Size(92, 27);
+            this.Res.TabIndex = 0;
+            this.Res.Text = "游戏结果";
+            // 
+            // GameFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "GameFrom";
+            this.Text = "GameFrom";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Rollpic)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label gus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Dice6;
+        private System.Windows.Forms.Button Dice3;
+        private System.Windows.Forms.Button Dice4;
+        private System.Windows.Forms.Button Dice5;
+        private System.Windows.Forms.Button Dice2;
+        private System.Windows.Forms.Button Dice1;
+        private System.Windows.Forms.Label gues;
+        private System.Windows.Forms.Label Res;
+        private System.Windows.Forms.PictureBox Rollpic;
+        private System.Windows.Forms.Label Num;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
