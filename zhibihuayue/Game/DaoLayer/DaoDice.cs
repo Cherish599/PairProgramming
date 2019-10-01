53143299c07c3738ce4e8d4261a3b407537c9374
zhibihuayue <wuxiandaizhi@163.com>
2019-10-01 21:45:10 +0800

    于丁+姜玖林

---
+++ zhibihuayue/Game/DaoLayer/DaoDice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entity;
+using DataBase;
+namespace DaoLayer
+{
+    class DaoDice
+    {
+        public class StudentDAO
+        {
+            public int getDiceRecordsNums()
+            {
+                List<Dice> DiceList = DiceData.getAll();
+                return DiceList.Count;
+            }
+
+            public List<Dice> getAllDice()
+            {
+                List<Dice> diceList = DiceData.getAll();
+                return diceList;
+            }
+
+        }
+    }
+}
